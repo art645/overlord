d070dfc3c91c501788d45e045a06c17ce791caee
art645 <aefanov95@inbox.ru>
2021-04-07 14:33:50 +0300

    first commit

---
+++ src/main/java/com/example/GreatLord/GreatLordApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreatLord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatLordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatLordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GreatLord/GreatLordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreatLord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatLordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
